3081dbb2eedd8e0e5cea696f032892d5b7968909
Avare Stewart <a.stewart@eos-solutions.com>
2019-01-31 02:01:57 +0100

    predictor for season

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+## initializing a default dictionary will means that even keys that do not yet exist will have this default value
+cities = defaultdict(lambda: 'OTHER')
+
+
+cities['madrid'] = 'this is madrid'
+cities['valencia'] = 'I am valencia'
+
+print(cities['madrid'])
+print(cities['valencia'])
+
+# note that the key torino is not present in the dictionary, and it will have the default value OTHER
+print(cities['torino'])
+
