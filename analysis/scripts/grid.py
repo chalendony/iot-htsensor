34d5d8b8ee563d99cb4ac24de3c3ae2828b8de21
Avare Stewart <a.stewart@eos-solutions.com>
2019-02-15 00:07:29 +0100

    model eval

--- analysis/scripts/grid.py
+++ analysis/scripts/grid.py
@@ -92,30 +92,6 @@ def grid_search(data, cfg_list, n_test, parallel=True):
     return scores
 
 
-# create a set of sarima configs to try
-def sarima_configs(seasonal=[0]):
-    models = list()
-    # define config lists
-    p_params = [0, 1, 2]
-    d_params = [0, 1]
-    q_params = [0, 1, 2]
-    t_params = ['n', 'c', 't', 'ct']
-    P_params = [0, 1, 2]
-    D_params = [0, 1]
-    Q_params = [0, 1, 2]
-    m_params = seasonal
-    # create config instances
-    for p in p_params:
-        for d in d_params:
-            for q in q_params:
-                for t in t_params:
-                    for P in P_params:
-                        for D in D_params:
-                            for Q in Q_params:
-                                for m in m_params:
-                                    cfg = [(p, d, q), (P, D, Q, m), t]
-                                    models.append(cfg)
-    return models
 
 
 if __name__ == '__main__':
